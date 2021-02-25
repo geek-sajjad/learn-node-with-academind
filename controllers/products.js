98faf8c225dc308b13510906b30c5bf8c20d3d8a
Sajad Sohrabi <sajjad.sohrabi2@gmail.com>
2021-02-25 22:25:48 +0330

    enhancing app, added new views

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     // res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
-    res.render('add-product', { docTitle: 'Add Product', path: '/admin/add-product' });
+    res.render('admin/add-product', { docTitle: 'Add Product', path: '/admin/add-product' });
 };
 
 exports.postAddProduct = (req, res, next) => {
@@ -14,7 +14,7 @@ exports.postAddProduct = (req, res, next) => {
 exports.getProducts = (req, res, next) => {
     // res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
     Product.fetchAll((products) => {
-        res.render('shop', { docTitle: 'Shop', products: products, path: '/' });
+        res.render('shop/products-list', { docTitle: 'Shop', products: products, path: '/' });
     });
 
 };
