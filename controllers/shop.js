ff0abc1e9dec1d8c8b743030b8b5d0a468dce9c8
Sajad Sohrabi <sajjad.sohrabi2@gmail.com>
2021-02-26 14:03:52 +0330

    add orders view

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,11 +1,17 @@
 const Product = require('../models/product');
 
 exports.getIndex = (req, res, next) => {
-    res.render('shop/index', { docTitle: 'Shop', path: '/' });
+    Product.fetchAll((products) => {
+        res.render('shop/index', { docTitle: 'Shop', path: '/', products: products });
+    });
 };
 
 exports.getCart = (req, res, next) => {
     res.render('shop/cart', { docTitle: 'Cart', path: '/cart' });
+};
+
+exports.getOrders = (req, res, next) => {
+    res.render('shop/orders', { docTitle: 'Orders', path: '/orders' });
 };
 
 exports.getProducts = (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -5,5 +5,6 @@ const shopController = require('../controllers/shop');
 router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
 router.get('/cart', shopController.getCart);
+router.get('/orders', shopController.getOrders);
 
 module.exports = router;
