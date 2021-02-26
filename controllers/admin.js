5fb96500ae1853625797a1d7e6691b45b8e85c18
Sajad Sohrabi <sajjad.sohrabi2@gmail.com>
2021-02-26 13:43:18 +0330

    added more filed for product

--- controllers/admin.js
+++ controllers/admin.js
@@ -9,7 +9,12 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
+    const title = req.body.title;
+    const price = req.body.price;
+    const description = req.body.description;
+    const imageUrl = req.body.imageUrl;
+
+    const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
 };
