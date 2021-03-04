5d992099861f1d43433412fc6c4e8a739725e250
Sajad Sohrabi <sajjad.sohrabi2@gmail.com>
2021-03-04 13:50:34 +0330

    added sequelize model

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ require('dotenv').config()
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const error = require('./controllers/error');
+const sequelize = require('./util/database');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,4 +18,9 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(error.get404);
 
-app.listen(3000);
+sequelize.sync()
+    .then(result => {
+        app.listen(3000);
+    }).catch(err => {
+        console.log(err);
+    });
--- util/database.js
+++ util/database.js
@@ -1,10 +1,7 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'academind',
-    password: process.env.MYSQL_PASSWORD
+const sequelize = new Sequelize('academind', 'root', process.env.MYSQL_PASSWORD, {
+    dialect: 'mysql'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
