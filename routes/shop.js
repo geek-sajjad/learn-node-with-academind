ba8c116eef4737a840d48225ef4f8be5a1eac68f
Sajad Sohrabi <sajjad.sohrabi2@gmail.com>
2021-03-11 15:12:37 +0330

    end of section mogodb driver

--- routes/shop.js
+++ routes/shop.js
@@ -7,9 +7,9 @@ router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
 router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
-// router.post('/cart-delete-item', shopController.postCartDeleteItem);
-// router.get('/orders', shopController.getOrders);
-// router.post('/create-order', shopController.postOrder);
+router.post('/cart-delete-item', shopController.postCartDeleteItem);
+router.get('/orders', shopController.getOrders);
+router.post('/create-order', shopController.postOrder);
 
 
 module.exports = router;
